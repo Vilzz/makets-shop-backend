627893d4eb9b7320cc7c0f4775e2b907a2540ff0
Vilzz <n79613928275@gmail.com>
2020-04-01 20:31:11 +0400

    Pagination added to api

--- controllers/makets.js
+++ controllers/makets.js
@@ -8,7 +8,7 @@ const Maket = require('../models/Maket');
 exports.getMakets = asyncHandler(async (req, res, next) => {
   let query;
   const reqQuery = { ...req.query };
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
   removeFields.forEach(param => delete reqQuery[param]);
   query = Maket.find(reqQuery);
 
@@ -21,11 +21,32 @@ exports.getMakets = asyncHandler(async (req, res, next) => {
     const sortBy = req.query.sort.split(',').join(' ');
     query = query.sort(sortBy);
   }
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Maket.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
 
   const makets = await query;
   res.status(200).json({
     success: true,
     count: makets.length,
+    pagination,
     data: makets
   });
 });
